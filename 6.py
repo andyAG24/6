6184f70e916675c354f4b788bdab2f9b42363009
andyAG24 <andy.ag.24@gmail.com>
2018-11-25 22:30:32 +0300

    Add files via upload

--- 6.py
+++ 6.py
@@ -1,20 +1,3 @@
-# Вы приехали помогать на ферму Дядюшки Джо и видите вокруг себя множество разных животных:
-
-# гусей "Серый" и "Белый"
-# корову "Маньку"
-# овец "Барашек" и "Кудрявый"
-# кур "Ко-Ко" и "Кукареку"
-# коз "Рога" и "Копыта"
-# и утку "Кряква"
-# Со всеми животными вам необходимо как-то взаимодействовать:
-
-# кормить
-# корову и коз доить
-# овец стричь
-# собирать яйца у кур, утки и гусей
-# различать по голосам(коровы мычат, утки крякают и т.д.)
-# Задача №1
-# Нужно реализовать классы животных, не забывая использовать наследование, определить общие методы взаимодействия с животными и дополнить их в дочерних классах, если потребуется.
 class Animal:
   def __init__(self, name, voice, weight):
     self.name = name
@@ -22,68 +5,66 @@ class Animal:
     self.weight = weight
   def feed(self, n):
     if n == 1:
-      return 'накормлено'
+      return 'РЅР°РєРѕСЂРјР»РµРЅРѕ'
     elif n == 0:
-      return 'голодный'
+      return 'РіРѕР»РѕРґРЅС‹Р№'
 
 class Gus(Animal):
-  animal_type = 'гусь'
+  animal_type = 'РіСѓСЃСЊ'
   def eggs(self, e):
     if e == 1:
-      return 'яйца собраны'
-gus_1 = Gus('Серый', 'га-га-га', 7)
-gus_2 = Gus('Белый', 'га-га-га', 7.5)
+      return 'СЏР№С†Р° СЃРѕР±СЂР°РЅС‹'
+gus_1 = Gus('РЎРµСЂС‹Р№', 'РіР°-РіР°-РіР°', 7)
+gus_2 = Gus('Р‘РµР»С‹Р№', 'РіР°-РіР°-РіР°', 7.5)
 
 class Cow(Animal):
-  animal_type = 'корова'
+  animal_type = 'РєРѕСЂРѕРІР°'
   def milk(self, m):
     if m == 1:
-      return 'подоено'
-cow_1 = Cow('Манька', 'мууу', 500)
+      return 'РїРѕРґРѕРµРЅРѕ'
+cow_1 = Cow('РњР°РЅСЊРєР°', 'РјСѓСѓСѓ', 500)
 
 class Sheep(Animal):
-  animal_type = 'овца'
+  animal_type = 'РѕРІС†Р°'
   def shave(self, s):
     if s == 1:
-      return 'пострижено'
-sheep_1 = Sheep('Барашек', 'беее', 100)
-sheep_2 = Sheep('Кудрявый', 'беее', 75)
+      return 'РїРѕСЃС‚СЂРёР¶РµРЅРѕ'
+sheep_1 = Sheep('Р‘Р°СЂР°С€РµРє', 'Р±РµРµРµ', 100)
+sheep_2 = Sheep('РљСѓРґСЂСЏРІС‹Р№', 'Р±РµРµРµ', 75)
 
 class Goat(Animal):
-  animal_type = 'коза'
+  animal_type = 'РєРѕР·Р°'
   def milk(self, m):
     if m == 1:
-      return 'подоено'
-goat_1 = Goat('Рога', 'меее', 40)
-goat_2 = Goat('Копыта', 'меее', 67)
+      return 'РїРѕРґРѕРµРЅРѕ'
+goat_1 = Goat('Р РѕРіР°', 'РјРµРµРµ', 40)
+goat_2 = Goat('РљРѕРїС‹С‚Р°', 'РјРµРµРµ', 67)
 
 class Chicken(Animal):
-  animal_type = 'курица'
+  animal_type = 'РєСѓСЂРёС†Р°'
   def eggs(self, e):
     if e == 1:
-      return 'яйца собраны'
-chicken_1 = Chicken('Ко-Ко', 'Ко-Ко', 3)
-chicken_2 = Chicken('Кукареку', 'Ко-Ко', 0.8)
+      return 'СЏР№С†Р° СЃРѕР±СЂР°РЅС‹'
+chicken_1 = Chicken('РљРѕ-РљРѕ', 'РљРѕ-РљРѕ', 3)
+chicken_2 = Chicken('РљСѓРєР°СЂРµРєСѓ', 'РљРѕ-РљРѕ', 0.8)
 
 class Duck(Animal):
-  animal_type = 'утка'
+  animal_type = 'СѓС‚РєР°'
   def eggs(self, e):
     if e == 1:
-      return 'яйца собраны'
-duck_1 = Duck('Кряква', 'кряяя', 1.6)
+      return 'СЏР№С†Р° СЃРѕР±СЂР°РЅС‹'
+duck_1 = Duck('РљСЂСЏРєРІР°', 'РєСЂСЏСЏСЏ', 1.6)
 
-print('Животное', gus_1.name, gus_1.animal_type, gus_1.feed(1), 'и', gus_2.eggs(1))
-print('Животное', gus_2.name, gus_2.animal_type, gus_2.feed(1), 'и', gus_2.eggs(1))
-print('Животное', cow_1.name, cow_1.animal_type, cow_1.feed(1), 'и', cow_1.milk(1))
-print('Животное', sheep_1.name, sheep_1.animal_type, sheep_1.feed(1), 'и', sheep_1.shave(1))
-print('Животное', sheep_2.name, sheep_2.animal_type, sheep_2.feed(1), 'и', sheep_2.shave(1))
-print('Животное', chicken_1.name, chicken_1.animal_type, chicken_1.feed(1), 'и', chicken_1.eggs(1))
-print('Животное', chicken_2.name, chicken_2.animal_type, chicken_2.feed(1), 'и', chicken_2.eggs(1))
-print('Животное', goat_1.name, goat_1.animal_type, goat_1.feed(1), 'и', goat_1.milk(1))
-print('Животное', goat_2.name, goat_2.animal_type, goat_2.feed(1), 'и', goat_2.milk(1))
-print('Животное', duck_1.name, duck_1.animal_type, duck_1.feed(1), 'и', duck_1.eggs(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', gus_1.name, gus_1.animal_type, gus_1.feed(1), 'Рё', gus_2.eggs(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', gus_2.name, gus_2.animal_type, gus_2.feed(1), 'Рё', gus_2.eggs(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', cow_1.name, cow_1.animal_type, cow_1.feed(1), 'Рё', cow_1.milk(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', sheep_1.name, sheep_1.animal_type, sheep_1.feed(1), 'Рё', sheep_1.shave(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', sheep_2.name, sheep_2.animal_type, sheep_2.feed(1), 'Рё', sheep_2.shave(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', chicken_1.name, chicken_1.animal_type, chicken_1.feed(1), 'Рё', chicken_1.eggs(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', chicken_2.name, chicken_2.animal_type, chicken_2.feed(1), 'Рё', chicken_2.eggs(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', goat_1.name, goat_1.animal_type, goat_1.feed(1), 'Рё', goat_1.milk(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', goat_2.name, goat_2.animal_type, goat_2.feed(1), 'Рё', goat_2.milk(1))
+print('Р–РёРІРѕС‚РЅРѕРµ', duck_1.name, duck_1.animal_type, duck_1.feed(1), 'Рё', duck_1.eggs(1))
 
-print('Вес всех животных:', gus_1.weight + gus_2.weight + cow_1.weight + sheep_1.weight + sheep_2.weight + chicken_1.weight +
-chicken_2.weight + goat_1.weight + goat_2.weight + duck_1.weight, 'кг')
-
-
+print('Р’РµСЃ РІСЃРµС… Р¶РёРІРѕС‚РЅС‹С…:', gus_1.weight + gus_2.weight + cow_1.weight + sheep_1.weight + sheep_2.weight + chicken_1.weight +
+chicken_2.weight + goat_1.weight + goat_2.weight + duck_1.weight, 'РєРі')
